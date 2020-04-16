7add6c8bd4095ac0834ec78a223f785ee3fdae5e
alexandell <alexandell@utec.edu.pe>
2020-04-16 17:04:05 -0500

    Solution for First class assignment

---
+++ week-1/day-4/first_class.rb
@@ -0,0 +1,13 @@
+class Square
+    def initialize(length)
+        @length = length
+    end
+
+    def area 
+        @length * @length
+    end
+
+end
+
+square_one = Square.new(10)
+puts square_one.area 
